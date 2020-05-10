8441a1e283cdd3f0930c4e6ec02c5ef3361c6eef
Jason Farrell <jason.farrell@newsignature.com>
2020-05-10 00:28:54 -0400

    removed import block

--- MyOrgApim/PeopleApi/infrastructure/apim/main.tf
+++ MyOrgApim/PeopleApi/infrastructure/apim/main.tf
@@ -53,8 +53,8 @@ resource "azurerm_api_management_api" "apim_people_api" {
   protocols           = [ "https" ]
   service_url         = var.app_service_base_url
 
-  import {
-    content_format    = "openapi-link"
-    content_value     = "${var.app_service_base_url}/swagger/v1/swagger.json"
-  }
+  #import {
+  #  content_format    = "openapi-link"
+  #  content_value     = "${var.app_service_base_url}/swagger/v1/swagger.json"
+  #}
 }
