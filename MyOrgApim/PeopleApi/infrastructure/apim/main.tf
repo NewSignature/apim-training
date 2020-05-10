b6b6ca3559f387d22f2d466d73cb58b35e94f05d
Jason Farrell <jason.farrell@newsignature.com>
2020-05-10 00:09:46 -0400

    adding in tf for api management

--- MyOrgApim/PeopleApi/infrastructure/apim/main.tf
+++ MyOrgApim/PeopleApi/infrastructure/apim/main.tf
@@ -1,5 +1,56 @@
 
 provider "azurerm" {
-  version = "=2.0.0"
+  version = "=2.1.0"
   features {}
+}
+
+terraform {
+    backend "azurerm" {
+        # filled in by azure devops runner
+    }
+}
+
+# collect variables
+variable "rg_name" {
+  type = string
+}
+
+
+variable "apim_name" {
+  type = string
+}
+
+variable "app_service_base_url" {
+  type = string
+}
+
+# resolve the products
+data "azurerm_api_management_product" "apim_product_std" {
+    product_id          = "${var.apim_name}-people-product-std"
+    api_management_name = var.apim_name
+    resource_group_name = var.rg_name
+}
+
+data "azurerm_api_management_product" "apim_product_unl" {
+    product_id              = "${var.apim_name}-people-product-unl"
+    api_management_name     = var.apim.name
+    resource_group_name     = var.rg.name
+}
+
+# get the resource that is our target API
+resource "azurerm_api_management_api" "apim_people_api" {
+  name                = "people-api"
+  resource_group_name = var.rg_name
+  api_management_name = var.apim_name
+
+  revision            = 1
+  display_name        = "People API"
+  path                = "people"
+  protocols           = [ "https" ]
+  service_url         = var.app_service_base_url
+
+  import {
+    content_format    = "swagger-link-json"
+    content_value     = "${var.app_service_base_url}/swagger/v1/swagger.json"
+  }
 }
